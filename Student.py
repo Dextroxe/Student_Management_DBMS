03bb4e0cb656beed627977d6e950c0d007763fee
dashmat <dashmathembram99@gmail.com>
2022-04-15 21:24:40 +0530

    progress 1

--- Student.py
+++ Student.py
@@ -6,8 +6,21 @@ class student :
         self.root.title("Student Management System")
         self.root.geometry("1350x700+0+0")
 
-        title=Label(self.root,text="Student Management System",font=("Times new roman",40,"bold"),bg="light blue",fg="black")
-        title.pack(side=TOP)
+        title=Label(self.root,text="Student Management System",bd=10,relief=GROOVE,font=("Times new roman",40,"bold"),bg="light blue",fg="black")
+        title.pack(side=TOP,fill=X)
+
+
+        Manage_Frame=Frame(self.root,bd=4,relief=RIDGE,bg="light green")
+        Manage_Frame.place(x=20,y=100,width=450,height=560)
+
+
+        Detail_Frame=Frame(self.root,bd=4,relief=RIDGE,bg="light green")
+        Detail_Frame.place(x=500,y=100,width=825,height=560)
+
+
+
+
+
 
 
 root=Tk()
